f78ad6623f538f92345a56d6de1d626664c7abad
earlysev7n <johnjerickagapito@gmail.com>
2025-06-25 16:34:34 +0800

    added human choice

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function getComputerChoice() {
     const choices = ["rock","paper","scissor"];
-    return choices[Math.floor(Math.random() * choices.length)]
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function getHumanChoice(){
+    let choice = prompt("Rock,Paper,Scissor? :")
+    return choice;
 }
 
 function playRound (humanChoice,computerChoice){
-
+    console.log("Human chose: ", humanChoice);
+    console.log("Computer chose: ",computerChoice);
 }
 
 const humanSelection = getHumanChoice();
