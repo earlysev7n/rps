b897ba843f779b5cac0599cf2d4016f861586784
earlysev7n <johnjerickagapito@gmail.com>
2025-06-25 16:58:49 +0800

    done

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const choices = ["rock","paper","scissor"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function getHumanChoice(){
-    let choice = prompt("Rock,Paper,Scissor? :")
+    let choice = prompt("Rock,Paper,Scissor? :").toLowerCase();
     return choice;
 }
 
 function playRound (humanChoice,computerChoice){
+    if (humanChoice == "rock" && computerChoice == "rock"){
+        console.log("Tied");
+    }else if (humanChoice == "paper" && computerChoice == "paper"){
+        console.log("Tied");
+    }else if (humanChoice == "scissor" && computerChoice == "scissor"){
+        console.log("Tied");
+    }else if (humanChoice == "rock" && computerChoice == "scissor"){
+        humanScore ++;
+        console.log("You won");
+    }else if (humanChoice == "paper" && computerChoice == "rock"){
+        humanScore ++;
+        console.log("You won");
+    }else if (humanChoice == "scissor" && computerChoice == "paper"){
+        humanScore ++;
+        console.log("You won");
+    }else if (humanChoice == "rock" && computerChoice == "paper"){
+        computerScore ++;
+        console.log("You lose");
+    }else if (humanChoice == "paper" && computerChoice == "scissor"){
+        computerScore ++;
+        console.log("You lose");
+    }else if (humanChoice == "scissor" && computerChoice == "rock"){
+        computerScore ++;
+        console.log("You lose");
+    }else{
+        console.log("Invalid input")
+    }
+
     console.log("Human chose: ", humanChoice);
     console.log("Computer chose: ",computerChoice);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+for (let i = 0; i < 5; i++){
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+}
 
-playRound(humanSelection, computerSelection)
+console.log("Final score, You:", humanScore, "Computer:",computerScore)
